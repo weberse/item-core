11274a9cc63b4d23a76fb8569a6646e25cffc248
Den <dfitsak@gmail.com>
2015-01-19 15:49:53 +0100

    tube task concat

--- item/tube/tasks/gulpfile.js
+++ item/tube/tasks/gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp')
     ,nodemon = require('gulp-nodemon')
     ,coffee = require('gulp-coffee')
+    ,concat = require('gulp-concat')
 
-gulp.task('tube', function () {
+gulp.task('tube', ['coffee'], function () {
   nodemon({ script: 'item/tube/server.js', ext: 'html js', ignore: ['ignored.js'] })
     .on('change', ['lint'])
     .on('restart', function () {
@@ -13,5 +14,6 @@ gulp.task('tube', function () {
 gulp.task('coffee', function() {
   gulp.src('./item/tube/src/client/*.coffee')
   .pipe(coffee({bare: true}))
+  .pipe(concat('app.js'))
   .pipe(gulp.dest('./item/tube/public/js/'))
 });
